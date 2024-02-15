f6c6dd09ce398be53b1a1ada929c32cb0598ca8a
AhmedRizwan23 <ahmedrixwan@gmail.com>
2024-02-15 16:32:53 +0500

    ..

--- lib/pages/intropage.dart
+++ lib/pages/intropage.dart
@@ -14,79 +14,73 @@ class Intropage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Lottie.asset("lib/assets/logo.json", height: 400),
-          Text(
-            "Minimal Shop",
-            style: GoogleFonts.dmSerifDisplay(
-                fontSize: 50,
-                color: Colors.brown
-                    .shade400), //TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
-          ),
-          const SizedBox(height: 10),
-          const Text(
-            "Premium Quality Products",
-            style: TextStyle(fontSize: 20),
-          ),
-          SizedBox(height: MediaQuery.of(context).size.height * 0.01),
-          Expanded(
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.onSecondary,
-                borderRadius: const BorderRadius.only(
-                  topRight: Radius.circular(100),
-                  topLeft: Radius.circular(100),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    offset: const Offset(2, -3),
-                    color: Colors.black.withOpacity(0.4),
-                    blurRadius: 50,
-                    spreadRadius: 30,
-                  ),
-                ],
-              ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 100),
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              "Minimal Shop",
+              style: GoogleFonts.dmSerifDisplay(
+                  fontSize: 50,
+                  color: Colors.brown
+                      .shade400), //TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
+            ),
+            const SizedBox(height: 5),
+            const Text(
+              "Premium Quality Products",
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 150),
+            Expanded(
               child: Column(
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20),
-                    child: Mycustombutton(
-                      width: 200,
-                      color: Theme.of(context).colorScheme.primary,
-                      onTap: () {
-                        Get.to(const SignInPage());
-                      },
-                      child: const Center(
-                        child: Text(
-                          "Login",
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
+                  Center(
+                    child: Container(
+                      width: 100,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16),
+                          border: Border.all(width: 1, color: Colors.black),
+                          color: const Color.fromARGB(255, 184, 71, 205)),
+                      child: Center(
+                        child: TextButton(
+                          onPressed: () {
+                            Get.to(const SignInPage());
+                          },
+                          child: const Text(
+                            "Login",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
                       ),
                     ),
                   ),
                   const Padding(
-                    padding: EdgeInsets.only(top: 20),
+                    padding: EdgeInsets.only(top: 5),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Text(
                           "or",
-                          style: TextStyle(fontSize: 20, color: Colors.white),
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
                         )
                       ],
                     ),
                   ),
                   Padding(
-                    padding: const EdgeInsets.only(top: 20),
+                    padding: const EdgeInsets.only(
+                      top: 20,
+                    ),
                     child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        const SizedBox(width: 115),
+                        // const SizedBox(width: 115),
                         Image.asset(
                           "lib/assets/facebook.png",
                           height: 60,
@@ -104,8 +98,18 @@ class Intropage extends StatelessWidget {
                 ],
               ),
             ),
-          ),
-        ],
+            Expanded(
+              child: SizedBox(
+                //  margin: const EdgeInsets.only(bottom: 30),
+                // height: 330,
+                //color: Colors.amber,
+                child: Lottie.asset(
+                  "lib/assets/logo.json",
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/themes/theme.dart
+++ lib/themes/theme.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 ThemeData lightmode = ThemeData(
   colorScheme: ColorScheme.light(
-    background: Colors.grey.shade300,
+    background: Colors.grey.shade200,
     primary: Colors.grey.shade200,
     secondary: Colors.white,
     inversePrimary: Colors.grey.shade700,
